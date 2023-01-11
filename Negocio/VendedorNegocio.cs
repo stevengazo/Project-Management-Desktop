e14475ce091fda87145cd866b8acb50ef6befba3
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2023-01-11 11:35:00 -0600

    Configuración Base del Proyecto

--- Negocio/VendedorNegocio.cs
+++ Negocio/VendedorNegocio.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datos;
+using Modelos;
 
 namespace Negocios
 {
-    public class Class1
-    {
-    }
+	internal class VendedorNegocio
+	{
+	}
 }
